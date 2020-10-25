646b85fb43ce6bd5b704b42ee17c59d7080289ca
lizhaochao <lzc@lizhaochao.com>
2020-10-25 08:58:17 +0800

    Use Enum.flat_map to replace Enum.map

--- lib/utils.ex
+++ lib/utils.ex
@@ -11,11 +11,10 @@ defmodule Utils do
 
   def do_m_to_kw(%{} = m) do
     m
-    |> Enum.map(fn {k, v} ->
+    |> Enum.flat_map(fn {k, v} ->
       []
       |> Keyword.put(k, v |> do_m_to_kw())
     end)
-    |> get_head()
   end
 
   def do_m_to_kw(v) when v |> is_bitstring() do
@@ -29,25 +28,15 @@ defmodule Utils do
   def do_m_to_kw([item | remain], acc) do
     result =
       item
-      |> Enum.map(fn {k, v} ->
+      |> Enum.flat_map(fn {k, v} ->
         []
         |> Keyword.put(k, v |> do_m_to_kw())
       end)
-      |> get_head()
 
     remain |> do_m_to_kw(acc ++ [result])
   end
 
   def do_m_to_kw([], acc) do
     acc
-  end
-
-  # -
-  defp get_head(list) when list |> length() > 0 do
-    list |> hd()
-  end
-
-  defp get_head(data) do
-    data
   end
 end
