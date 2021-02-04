677dd34b13661f755e0a27eeeaa6540aa1d78989
lizhaochao <lzc@lizhaochao.com>
2021-02-04 15:57:48 +0800

    0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Argx.MixProject do
   def project do
     [
       app: :argx,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.11",
       aliases: aliases(),
       description: description(),
