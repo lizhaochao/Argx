7565873629153ebdab37a0bebef6f93e00cc5162
lizhaochao <lzc@lizhaochao.com>
2021-02-09 10:42:38 +0800

    format doc

--- lib/argx.ex
+++ lib/argx.ex
@@ -1,34 +1,34 @@
 defmodule Argx do
   @moduledoc """
 
-  Using DSLs to check function args.
+  A DSL for validating function's args.
 
   including 5 functionalities:
-  1. set default value if arg is nil.
-  2. convert arg's value automatically, if arg's value is compatible, like: "1" to 1.
-  3. check whether arg is lacked
-  4. check whether arg's type is error
-  5. check whether arg's length/value is out of range
+  - set default value if arg is ```nil```.
+  - convert arg's value automatically, if arg's value is compatible, like: ```"1"``` to ```1```.
+  - check whether arg is lacked
+  - check whether arg's type is error
+  - check whether arg's length/value is out of range
 
   ### Validator
   We can define a validator as follows:
 
-  ~~~
+  ```
   defmodule Project.Util.Validator do
     use Argx
   end
-  ~~~
+  ```
 
   ### Usage 1:
 
-  1. configs keyword is necessary
-  2. :string declare arg's data type.
-  3. :optional declare arg's value that can be nil.
-  4. :auto declare that lib convert it to integer value automatically if it is compatible.
-  5. 10..20 means arg value should be in 10 to 20, not equal 10 or not equal 20.
-  6. || operator is used to define default value, it also be a string/number/function.
+  - ```configs``` keyword is necessary
+  - ```:string``` declare arg's data type.
+  - ```:optional``` declare arg's value that can be nil.
+  - ```:auto``` declare that lib convert it to integer value automatically if it is compatible.
+  - ```10..20``` means arg value should be in 10 to 20, not equal 10 or not equal 20.
+  - ```||``` operator is used to define default value, it also be a value/function.
 
-  ~~~
+  ```
   defmodule Project.A.B do
     import Project.Util.Validator
 
@@ -41,18 +41,18 @@ defmodule Argx do
       end
     end
   end
-  ~~~
+  ```
 
   ### Usage 2 :
 
   defconfig also is a DSL that define the checking rule,
   including 2 parts:
-  1. config name, must be an atom
-  2. config rule, like: field_name(:string, :optional, :auto, 1..10) || 1
+  - config name, must be an atom
+  - config rule, like: ```arg_name(:string, :optional, :auto, 1..10) || 1```
 
   we can reuse config rule by config name.
 
-  ~~~
+  ```
   defmodule Project.A.B do
     import Project.Util.Validator
 
@@ -64,12 +64,14 @@ defmodule Argx do
       end
     end
   end
-  ~~~
+  ```
 
   ### Callback
 
+  ```format_errors/1```
+
   we can define in 2 places.
-  ~~~
+  ```
   defmodule Project.A.B do
     ...
 
@@ -84,45 +86,44 @@ defmodule Argx do
       errors
     end
   end
-  ~~~
+  ```
 
-  ~~~
+  ```
   defmodule Project.Util.Validator do
     use Argx
 
     def format_errors(errors) do
-      errors
+      {:error, 1000, errors}
     end
   end
-  ~~~
-
+  ```
 
   ### All DSLs
-  1. defconfig
-  2. with_check
+  - ```defconfig```
+  - ```with_check```
 
   ### All Checking Data Type Values
-  1.  :string
-  2.  :integer
-  3.  :float
-  4.  :list
-  5.  :map
+  -  ```:string```
+  -  ```:integer```
+  -  ```:float```
+  -  ```:list```
+  -  ```:map```
 
   ### Available Range format
-  1. 1..10 (1 to 10, not equal 1 or not equal 10)
-  2. 10 (10 to 10, equal to 10)
+  - ```1..10``` (1 to 10, not equal 1 or not equal 10)
+  - ```10``` (10 to 10, equal to 10)
 
   you can apply range setting to all data types.
 
   ### Available Default value format
-  1. value, like: 1, 1.1, "default"
-  2. function, like: Module.A.B.get_ts() or get_ts()
+  - value, like: ```1```, ```1.1```, ```"default"```
+  - function, like: ```Module.A.B.get_ts()``` or ```get_ts()```
 
-  not support fn function at present.
+  not support ```fn``` function at present.
 
   ### Functionalities
-  1. :optional
-  2. :auto, including 3 situations:
+  - ```:optional```
+  - ```:auto```, including 3 situations:
   (1). string to integer
   (2). string to float
   (3). integer to float
