f6d1937b0f280e88510d77340bfc7be1b1c3839a
lizhaochao <lzc@lizhaochao.com>
2021-04-23 23:48:57 +0800

    improve Argx.Util

--- lib/util.ex
+++ lib/util.ex
@@ -12,25 +12,6 @@ defmodule Argx.Util do
   end
 
   ###
-  def make_fun_name(name) do
-    name
-    |> fun_name_rule()
-    |> to_fun_name()
-  end
-
-  defp fun_name_rule(name) when is_bitstring(name), do: ["__", name, "__"]
-  defp fun_name_rule(name) when is_atom(name), do: name |> to_string() |> fun_name_rule()
-  defp fun_name_rule(_), do: []
-
-  def to_fun_name(parts) do
-    parts
-    |> Enum.map(fn part -> to_string(part) end)
-    |> IO.iodata_to_binary()
-    |> String.downcase()
-    |> String.to_atom()
-  end
-
-  ###
   def make_module_name([_ | _] = parts) do
     [Elixir | parts]
     |> Enum.map(fn part -> to_string(part) end)
@@ -39,6 +20,19 @@ defmodule Argx.Util do
   end
 
   def make_module_name(_other_parts), do: nil
+
+  def make_fun_name(name) do
+    name
+    |> fun_name_rule()
+    |> Enum.map(fn part -> to_string(part) end)
+    |> IO.iodata_to_binary()
+    |> String.downcase()
+    |> String.to_atom()
+  end
+
+  defp fun_name_rule(name) when is_bitstring(name), do: ["__", name, "__"]
+  defp fun_name_rule(name) when is_atom(name), do: name |> to_string() |> fun_name_rule()
+  defp fun_name_rule(_), do: []
 
   ###
   def sort_by_keys(keyword, keys) when is_list(keyword) do
