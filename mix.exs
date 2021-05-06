b89526640a1c9285d82a21d7cec69aacab08109c
lizhaochao <lzc@lizhaochao.com>
2021-05-06 23:20:50 +0800

    fix test_pattern

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Argx.MixProject do
       package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      test_pattern: "*_test.ex",
+      test_pattern: "*_test.exs",
 
       # Docs
       name: "Argx",
