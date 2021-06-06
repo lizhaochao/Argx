d75c565afc7f72d6665e68be167bdd2ac36a2e99
lizhaochao <lzc@lizhaochao.com>
2021-06-06 17:47:19 +0800

    set deps version

--- mix.exs
+++ mix.exs
@@ -1,57 +1,52 @@
 defmodule Argx.MixProject do
   use Mix.Project
 
+  @description "DSLs for checking args"
+
+  @gitee_repo_url "https://gitee.com/lizhaochao/argx"
+  @github_repo_url "https://github.com/lizhaochao/Argx"
+
+  @version "1.1.1"
+
   def project do
     [
       app: :argx,
-      version: "1.1.1",
+      version: @version,
       elixir: "~> 1.11",
-      aliases: aliases(),
-      description: description(),
-      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      aliases: aliases(),
+
+      # Test
       test_pattern: "*_test.exs",
+
+      # Hex
+      package: package(),
+      description: @description,
 
       # Docs
       name: "Argx",
-      docs: [
-        main: "Argx"
-      ]
+      docs: [main: "Argx"]
     ]
   end
 
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
+  def application, do: [extra_applications: [:logger]]
 
   defp deps do
     [
-      {:credo, "> 0.0.0", only: [:dev, :test], runtime: false},
-      {:ex_doc, "> 0.0.0", only: :dev, runtime: false}
+      {:credo, "~> 1.5.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.24.2", only: :dev, runtime: false}
     ]
-  end
-
-  defp description do
-    "DSLs for checking args"
   end
 
   defp package do
     [
       name: "argx",
+      maintainers: ["lizhaochao"],
       licenses: ["MIT"],
-      links: %{
-        "Gitee" => "https://gitee.com/lizhaochao/argx",
-        "GitHub" => "https://github.com/lizhaochao/Argx"
-      }
+      links: %{"Gitee" => @gitee_repo_url, "GitHub" => @github_repo_url}
     ]
   end
 
-  defp aliases do
-    [
-      test: ["format", "test"]
-    ]
-  end
+  defp aliases, do: [test: ["format", "test"]]
 end
